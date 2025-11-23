ddf903ec7503388f72f235a28fe310ee1b9f2931
NikolVasileva <nikol.vasileva9@gmail.com>
2025-11-23 15:14:01 +0200

    Add Register Page

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -9,12 +9,12 @@ export default function Header() {
                 {/* <!-- Logged-in users --> */}
                 <div id="user">
                     <Link to="/games/create">Add Game</Link>
-                    <Link to="#">Logout</Link>
+                    <Link to="/logout">Logout</Link>
                 </div>
                 {/* <!-- Guest users --> */}
                 <div id="guest">
                     <Link to="#">Login</Link>
-                    <Link to="#">Register</Link>
+                    <Link to="/register">Register</Link>
                 </div>
             </nav>
         </header>
